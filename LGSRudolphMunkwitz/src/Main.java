27b237038d6c043a70aec1aa436c533828fe6b25
JanneM96 <jmusterfrau@gmail.com>
2015-09-14 11:01:20 +0200

    filemanager

--- LGSRudolphMunkwitz/src/Main.java
+++ LGSRudolphMunkwitz/src/Main.java
@@ -1,12 +1,29 @@
+import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			//test
+		Filemanager fm = new Filemanager();  
+        
+       
+        String filename = "C:/Users/Alexander/workspace/aop1pruefung/test.txt";  
+          
+        try  
+        {  
+            String[] lines = fm.readLines(filename);  
+          
+            for (String line : lines)   
+            {  
+                System.out.println(line);  
+            }  
+        }  
+        catch(IOException e)  
+        {  
+            
+            System.out.println("Unable to create "+filename+": "+e.getMessage());                
+        }  
+    }  
 		
-	}
-	void meeeep(){
-		
-	}
 }
