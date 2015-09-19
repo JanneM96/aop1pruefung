62d6a501b8a182dd7a93768ee85bfd263e291ea2
JanneM96 <jmusterfrau@gmail.com>
2015-09-19 16:12:48 +0200

    auskommentiert

--- LGSRudolphMunkwitz/src/Main.java
+++ LGSRudolphMunkwitz/src/Main.java
@@ -5,13 +5,13 @@ public class Main {
 	public static void main(String[] args) {
         Mathe mathe = new Mathe();
 
-       //#finger weg <3
+       /*//#finger weg <3
         //unittest für mathe mit x-ergebnissen 1, -2 und 3
         double[][] matrix;
         matrix = new double[][]{{1, 2, 3, 6},{0, 1, 4, 10},{0, 0, 1, 3}};
 
         Mathe.ablauf(matrix);
-        //#finger weg <3
+        *///#finger weg <3
 
 		Filemanager fm = new Filemanager();
         String filename = "C:/Users/Alexander/workspace/aop1pruefung/test.txt";  
